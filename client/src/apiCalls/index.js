9bcc0df406cc9109887923c9aa06f2ca0471019c
Dev Arora <devro3014@gmail.com>
2024-03-26 01:20:27 +0530

    Integrate Gpt Search

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,3 +1,3 @@
 const API = process.env.PUBLIC_SERVER_URL;
 export default API;
-export const OPENAI_KEY = "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";
+export const OPENAI_KEY = process.env.OPENAI_KEY;
